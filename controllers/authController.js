70834f56abdd2ea50f6ee1e7959527de2f71315f
Maimit Patel <maimitp9@gmail.com>
2017-10-11 00:31:56 +0530

    change responce while login

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,13 +8,13 @@ module.exports.login = (req, res) => {
   User.findOne({email: req.body.email}, (err, user) => {
     if (err) throw err;
     if(!user){
-      return res.status(200).send({ susscess: false, message: "User Not Found"})
+      return res.status(203).send({ authenticated: false, message: "User Not Found"})
     }else{
       if(validPassword(req.body.password, user.password)){
         var token = jwt.sign({id: user._id}, config.secret);
-        res.status(200).send({success: true, token: token, user: userHelper.userInfo(user) })
+        res.status(200).send({authenticated: true, token: token, user: userHelper.userInfo(user) })
       }else{
-        res.status(200).send({success: false, message: "Incorrect Password"})
+        res.status(203).send({authenticated: false, message: "Incorrect Password"})
       }
     }
   })
@@ -23,16 +23,16 @@ module.exports.login = (req, res) => {
 module.exports.authMe = (req, res) => {
   var token = req.headers['authorization']
   if(!token){
-    return res.status(401).send({ success: false, message: 'Token not providesd' })
+    return res.status(203).send({ authenticated: false, message: 'Token not providesd' })
   }
 
   jwt.verify(token, config.secret, (err, decoded) => {
     if(err)
-      return res.status(500).send({ success: false, message: 'failed to authenticate token'})
+      return res.status(203).send({ authenticated: false, message: 'failed to authenticate token'})
     User.getUser({_id: decoded.id}, (err, user) => {
-      if (err) return res.status(500).send("There was a problem finding the user.");
-      if(!user) return res.status(500).send({success: false, message: 'User not found'});
-      res.status(200).send({success: true, user: userHelper.userInfo(user)})
+      if (err) return res.status(203).send("There was a problem finding the user.");
+      if(!user) return res.status(203).send({authenticated: false, message: 'User not found'});
+      res.status(200).send({authenticated: true, user: userHelper.userInfo(user)})
     })
   })
 }
